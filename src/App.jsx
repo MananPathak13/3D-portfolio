7f75e87ef73680815f76bb36b5ce96b3ce553dc0
MananPathak13 <59965607+MananPathak13@users.noreply.github.com>
2024-03-17 08:52:40 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter } from "react-router-dom";
 
-// eslint-disable-next-line no-unused-vars
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 
 const App = () => {
@@ -15,7 +14,7 @@ const App = () => {
         <Experience />
         <Tech />
         <Works />
-        {/* <Feedbacks /> */}
+        <Feedbacks />
         <div className='relative z-0'>
           <Contact />
           <StarsCanvas />
